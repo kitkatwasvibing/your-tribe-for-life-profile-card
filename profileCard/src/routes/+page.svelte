8580b3c24c045a2334f64c4339232a3fc4b93380
Kitkatisvibing <kittykatekawaii@gmail.com>
2026-09-02 16:49:26 +0200

    data in svelte

--- profileCard/src/routes/+page.svelte
+++ profileCard/src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script>
+    let {data} = $props();
+    const persons = data.persons;
+</script>
+
+{#each persons as person}
+    {person.name} <br>
+{/each}
 <h1>test</h1>
 <p>it workssss</p>
+
+<style>
+
+</style>
