e0f6c8c3f93b2f6130c54dd1d1dfd0b22c73bc34
Kitkatisvibing <kittykatekawaii@gmail.com>
2026-09-18 14:20:16 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,28 +2,45 @@
     let {data} = $props();
     const person = data.person;
 </script>
+<div>
+    <h1>{person.name}'s Page!</h1>
 
-<h1>{person.name}'s Page!</h1>
+    <article>
+    <h2>Whats all this then?</h2>
+    </article>
 
-<article>
-<h2>Whats all this then?</h2>
-</article>
+    <main>
+        <h2>Be informed about me</h2>
+        <img src={`https://fdnd.directus.app/assets/${person.mugshot_year2}`} alt="this is what i look like" width="200" height="auto">
+    <ul>
+        <li>birthdate: {person.birthdate}</li>
+        <li>Github: <a href="https://github.com/Kitkatwasvibing">{person.github_handle}</a></li>
+        <li>{person.bio}</li>
+    </ul>
+    </main>
 
-<main>
-<h2>Be informed about me</h2>
-</main>
-
-<article>
-<h2>My fav webbed sites</h2>
-</article>
+    <article>
+    <h2>My fav webbed sites</h2>
+    </article>
+</div>
 
 <style>
+    div{
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 10px;
+        overflow-x: hidden;
+    }
     article{
         border: 5px solid darkblue;
         border-radius: 30px;
+        width: 100vw;
     }
 
     main{
         border: 30px dotted cadetblue;
+        width: fit-content;
     }
 </style>
